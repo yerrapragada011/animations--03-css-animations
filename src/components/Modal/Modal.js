ba843ae64f3de2a4237a6b2347f2f817a51912f9
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-22 15:30:48 -0400

    Transition Events

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,9 +3,19 @@ import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
 const modal = (props) => {
   return (
-    <Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+    >
       {(state) => {
         const cssClasses = [
           'Modal',
