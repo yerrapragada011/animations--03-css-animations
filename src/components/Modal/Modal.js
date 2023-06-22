4dd7a21e6c744c1f63ecd85f4b9f93bebc425e4e
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-22 15:17:38 -0400

    Using the Transition Component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import "./Modal.css";
+import './Modal.css';
 
-const modal = props => {
+const modal = (props) => {
   const cssClasses = [
-    "Modal",
-    props.show ? "ModalOpen" : "ModalClosed"
+    'Modal',
+    props.show === 'entering'
+      ? 'ModalOpen'
+      : props.show === 'exiting'
+      ? 'ModalClosed'
+      : null,
   ];
 
   return (
     <div className={cssClasses.join(' ')}>
       <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
+      <button className='Button' onClick={props.closed}>
         Dismiss
       </button>
     </div>
